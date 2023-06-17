aa79e8d88c2e239d854fdc23e43d572cb6980249
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-06-17 12:45:47 +0700

    add migration order

---
+++ database/migrations/2023_06_17_053114_create_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->integer("buyer_id")->nullable();
+            $table->integer("freelancer_id")->nullable();
+            $table->integer("service_id")->nullable();
+            $table->longText("file")->nullable();
+            $table->longText("note")->nullable();
+            $table->date("expired")->nullable();
+            $table->integer("order_status_id")->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
