12f20336c8353e5f9628e966a27aea586127316c
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-10-01 22:13:54 +0700

    add sweet alert and function update etc

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -5,14 +5,44 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\Dashboard\Profile\UpdateProfileRequest;
+use App\Http\Requests\Dashboard\Profile\UpdateDetailUserRequest;
+
+use Illuminate\Support\Facades\Storage;
+use Iluminate\Support\Facades\DB;
+
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Http\Testing\File;
+// use Illuminate\Http\File;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use App\Models\DetailUser;
+use App\Models\ExperienceUser;
+
 class ProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        //
+        $user = User::where('id', Auth::user()->id)->first();
+        $experience_user = ExperienceUser::where('detail_user_id', $user->detail_user->id)
+            ->orderBy('id', 'asc')
+            ->get();
+        // $detail_user = DetailUser::where('users_id', $user->detail_user->id)->get();
+
+
+        return view('pages.dashboard.profile', compact('user', 'experience_user'));
     }
 
     /**
@@ -20,7 +50,7 @@ class ProfileController extends Controller
      */
     public function create()
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -28,7 +58,7 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -36,7 +66,7 @@ class ProfileController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -44,15 +74,74 @@ class ProfileController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return abort(404);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProfileRequest $request_profile, UpdateDetailUserRequest $request_detail_user)
     {
-        //
+        $data_profile = $request_profile->all();
+        $data_detail_user = $request_detail_user->all();
+
+
+        // get foto
+        $get_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+
+        // delete old file from storage
+        if (isset($data_detail_user['photo'])) {
+            $data = 'storage' . $get_photo['photo'];
+
+            if (File::exists($data)) {
+                File::delete($data);
+            } else {
+                File::delete('storage/app/public' . $get_photo['photo']);
+            }
+        }
+
+
+        // store file to storage
+        if (isset($data_detail_user['photo'])) {
+            $data_detail_user['photo'] = $request_detail_user->file('photo')->store(
+                'assets/photo',
+                'public'
+            );
+        }
+
+        // proses save
+        $user = User::find(Auth::user()->id);
+        $user->update($data_profile);
+
+        // proses save to detail user
+        $detail_user = DetailUser::find($user->detail_user->id);
+        $detail_user->update($data_detail_user);
+
+        // proses to save experience user
+        $experience_user_id = ExperienceUser::where('detail_user_id', $detail_user['id'])->first();
+
+        if (isset($experience_user_id)) {
+            foreach ($data_profile['experience'] as $key => $value) {
+                $experience_user = ExperienceUser::find($key);
+                $experience_user->detail_user_id = $detail_user['id'];
+                $experience_user->experience = $value;
+                $experience_user->save();
+            }
+        } else {
+            foreach ($data_profile['experience'] as $key => $value) {
+                if (isset($value)) {
+                    $experience_user = new ExperienceUser;
+                    $experience_user->detail_user_id = $detail_user['id'];
+                    $experience_user->experience = $value;
+                    $experience_user->save();
+                }
+            }
+        }
+
+        Alert::toast()->success('update has been success');
+        // Alert::success('Success Title', 'Success Message');
+
+        return back();
     }
 
     /**
@@ -60,6 +149,32 @@ class ProfileController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        return abort(404);
+    }
+
+    // custom
+    public function delete()
+    {
+        // get user 
+        $get_user_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+        $path_photo = $get_user_photo['photo'];
+
+
+        $data = DetailUser::find($get_user_photo['id']);
+        $data->photo = NULL;
+        $data->save();
+
+        // delete
+        $data = 'storage/'. $path_photo;
+        if(File::exist($data)){
+            File::delete($data);
+        }else{
+            File::delete('storage/app/public/'. $path_photo);
+        }
+
+        Alert::toast()->success('delete has been success');
+        return back();
+
+
     }
 }
--- app/Http/Controllers/Dashboard/ServiceController.php
+++ app/Http/Controllers/Dashboard/ServiceController.php
@@ -5,14 +5,44 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\Dashboard\Service\StoreServiceRequest;
+use App\Http\Requests\Dashboard\Service\UpdateServiceRequest;
+
+use Illuminate\Support\Facades\Storage;
+use Iluminate\Support\Facades\DB;
+
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Http\Testing\File;
+// use Illuminate\Http\File;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Service;
+use App\Models\AdvantageService;
+use App\Models\Tagline;
+use App\Models\AdvantageUser;
+use App\Models\ThumbnailService;
+use App\Models\Order;
+use App\Models\User;
+
+
 class ServiceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
-        return view('pages.dashboard.service.index');
+
+        $services = Service::where('users_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+
+        return view('pages.dashboard.service.index', compact('services'));
     }
 
     /**
@@ -26,9 +56,58 @@ class ServiceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreServiceRequest $request)
     {
-        //
+        $data = $request->all();
+
+        // dd($data);
+
+        $data['users_id'] = Auth::user()->id;
+
+        $service = Service::creat($data);
+
+        // add to advantage
+
+        foreach ($data['advantage-service'] as $key => $value) {
+            $advantage_service = new AdvantageService;
+            $advantage_service->service_id = $service->id;
+            $advantage_service->advantage_id = $value;
+            $advantage_service->save();
+        }
+
+        // add advantage user
+        foreach ($data['advantage-user'] as $key => $value) {
+            $advantage_user = new AdvantageService;
+            $advantage_user->service_id = $service->id;
+            $advantage_user->advantage_id = $value;
+            $advantage_user->save();
+        }
+
+        // add to thumbnail
+        if ($request->hasFile('thumbnail')) {
+            foreach ($request->file('thumbnail') as $file) {
+                $path = $file->store(
+                    'assets/service/thumbnail',
+                    'public'
+                );
+
+                $thumbnail_service = new ThumbnailService;
+                $thumbnail_service->service_id = $service['id'];
+                $thumbnail_service->thumbnail = $path;
+                $thumbnail_service->save();
+            }
+        }
+
+        // add to tagline
+
+        foreach ($data['tagline'] as $key => $value) {
+            $tagline = new Tagline;
+            $tagline->service_id = $service['id'];
+            $tagline->tagline = $value;
+            $tagline->save();
+        }
+
+        Alert::toast()->success('Save has been success');
     }
 
     /**
@@ -36,23 +115,136 @@ class ServiceController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return abort(404);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Service $service)
     {
-        return view('pages.dashboard.service.edit');
+
+        $advantage_service = AdvantageService::where('service_id', $service->id)->get();
+        $tagline = Tagline::where('service_id', $service['id'])->get();
+        $advantage_user = AdvantageUser::where('service_id', $service['id'])->get();
+        $thumbnail_service = ThumbnailService::where('service_id', $service['id'])->get();
+
+        return view('pages.dashboard.service.edit', compact('service', 'advantage_service', 'tagline', 'advantage_user', 'thumbnail_service'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateServiceRequest $request, Service $service)
     {
-        //
+        $data = $request->all();
+        $service->update($data);
+
+
+        // update to advantage service
+        foreach ($data['advantage-service'] as $key => $value) {
+            $advantage_service = AdvantageService::find($key);
+
+            // $advantage_service->service_id = $service['id'];
+            $advantage_service->advantage = $value;
+            $advantage_service->save();
+        }
+
+        // update to advantage service
+        if (isset($data['advantage_service'])) {
+            foreach ($data['advantage-service'] as $key => $value) {
+                $advantage_service = AdvantageService::find($key);
+                $advantage_service->service_id = $service['id'];
+                $advantage_service->advantage = $value;
+                $advantage_service->save();
+            }
+        }
+
+        // update to advantage user
+        foreach ($data['advantage-user'] as $key => $value) {
+            $advantage_user = AdvantageUser::find($key);
+
+            // $advantage_user->service_id = $service['id'];
+            $advantage_user->advantage = $value;
+            $advantage_user->save();
+        }
+
+        // update to advantage user
+        if (isset($data['advantage_user'])) {
+            foreach ($data['advantage-user'] as $key => $value) {
+                $advantage_user = AdvantageUser::find($key);
+                $advantage_user->service_id = $service['id'];
+                $advantage_user->advantage = $value;
+                $advantage_user->save();
+            }
+        }
+
+
+        // update to tagline
+        foreach ($data['tagline'] as $key => $value) {
+            $tagline = Tagline::find($key);
+
+            // $tagline->service_id = $service['id'];
+            $tagline->tagline = $value;
+            $tagline->save();
+        }
+
+        // update to advantage user
+        if (isset($data['tagline'])) {
+            foreach ($data['tagline'] as $key => $value) {
+                $tagline = Tagline::find($key);
+                $tagline->service_id = $service['id'];
+                $tagline->advantage = $value;
+                $tagline->save();
+            }
+        }
+
+        // update thumbnail service
+        if ($request->hasFile('thumbnails')) {
+            foreach ($request->file('thumbnails') as $key => $value) {
+                // get old photo
+                $get_photo = ThumbnailService::where('id', $key)->first();
+
+                // store photo
+                // file
+                $path = $get_photo->store(
+                    'assets/service/thumbnail',
+                    'public'
+                );
+
+
+                // update thumbnail
+
+                $thumbnail_service = ThumbnailService::find($key);
+                $thumbnail_service->thumbnail = $path;
+                $thumbnail_service->save();
+
+
+                // delete old photo thumbnail
+                $data = 'storage' . $get_photo['photo'];
+                if (File::exists($data)) {
+                    FIle::delete($data);
+                } else {
+                    File::delete('storage/app/public/' . $get_photo['photo']);
+                }
+            }
+        }
+
+        if ($request->hasFile('thumbnail')) {
+            foreach ($request->file('thumbnail') as $key => $value) {
+                $path = $get_photo->store(
+                    'assets/service/thumbnail',
+                    'public'
+                );
+
+                $thumbnail_service = new ThumbnailService;
+                $thumbnail_service->service_id['id'];
+                $thumbnail_service->thumbnail = $path;
+                $thumbnail_service->save();
+            }
+        }
+
+        Alert::toast()->success('Update has been success');
     }
 
     /**
